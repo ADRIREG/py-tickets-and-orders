a9a8e61ba929159c845caad471995d746c05494b
Adrianna Regulska <adrianna.regulska.1@gmail.com>
2026-02-07 21:54:25 +0100

    Solution

--- scan.py
+++ scan.py
@@ -1,13 +1,13 @@
 import os
 
-for root, dirs, files in os.walk('.'):
-    if 'venv' in root or '__pycache__' in root:
+for root, dirs, files in os.walk("."):
+    if "venv" in root or "__pycache__" in root:
         continue
-    for f in files:
-        path = os.path.join(root, f)
+    for filename in files:
+        path = os.path.join(root, filename)
         try:
-            with open(path, 'rb') as fh:
-                if b'\x00' in fh.read():
+            with open(path, "rb") as fh:
+                if b"\x00" in fh.read():
                     print(path)
         except Exception:
             pass
